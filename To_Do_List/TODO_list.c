8f7e54405a119b9d890ec97e056b220b72393268
Pablo G. Blando <pablo.gonzalezblando@gmail.com>
2025-12-12 20:37:44 -0600

    first commit

---
+++ To_Do_List/TODO_list.c
@@ -0,0 +1,20 @@
+/*
+  Author: Pablo G. Blando
+  Date: Dec 12 ~ Dec ??
+  Program: To Do List
+
+  Resume: This program atempts to do a simple To Do List with the following specs:
+  
+  + Can write To Do Tasks in a plain text file.
+  + Can load and save To Do Lists in plain text files.
+  + Can Delete To Do Tasks from a load or in creation time file.
+  + Can set To Do Tasks as completed without delete the entry.
+*/
+
+#include<stdio.h>
+
+int main(){
+    
+
+    return 0;
+}
